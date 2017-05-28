8c7a4b4859a68e7618957bbdacee3c51a035160d
angelicacomoti <angelica_comoti@hotmail.com>
2017-05-28 19:41:54 -0300

    arrumando juntando os dois projetos

--- ClientGUI/src/Clients/ReaderDynamicGUI.java
+++ ClientGUI/src/Clients/ReaderDynamicGUI.java
@@ -1,31 +1,22 @@
 package Clients;
 
-<<<<<<< HEAD
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
+import File.Reader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-=======
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
->>>>>>> master
 import javafx.scene.control.CustomMenuItem;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-<<<<<<< HEAD
-import javafx.scene.shape.Path;
-=======
->>>>>>> master
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -33,10 +24,11 @@ public class ReaderDynamicGUI {
 	
 	private Stage janela;
 	private MenuBar menuBar;
+	private Menu menuConfig;
 	private Menu menuFile;
 	private Menu menuTime;
 	private TextField timeTxt;
-	private TextField fileTxt;
+	private ComboBox<String> combo;
 	private GridPane grid;
 	private TextField visor;
 	private Button bttPlay;
@@ -44,6 +36,8 @@ public class ReaderDynamicGUI {
 	private Button bttStop;
 	private HBox bttContainer;
 	
+	private String message;
+	private List<String> files;
 	
 	public void createView(Stage janela){
 		this.janela = janela;
@@ -64,129 +58,90 @@ public class ReaderDynamicGUI {
 	
 	private MenuBar createMenuBar(MenuBar menuBar){
 		this.menuBar = menuBar;
-		Menu menuConfig = new Menu("Config");
+		menuConfig = new Menu("Config");
 		menuConfig.getItems().addAll(createMenuFile(), createMenuTime());
 		this.menuBar.getMenus().addAll(menuConfig);
 		return this.menuBar;
 	}
-	
-<<<<<<< HEAD
-	private CustomMenuItem createCustomMenuItem(ComboBox combo){
+
+	private CustomMenuItem createCustomComboBox(ComboBox<String> combo){
 		CustomMenuItem customMenuFileItem = new CustomMenuItem(combo);
-=======
-	private CustomMenuItem createCustomMenuItem(TextField txt){
-		CustomMenuItem customMenuFileItem = new CustomMenuItem(txt);
->>>>>>> master
 		customMenuFileItem.setHideOnClick(false);
 		return customMenuFileItem;
 	}
 	
-	//clear the text time when the mouse clicks on it
+	private ComboBox<String> createComboBox(ComboBox<String> combo){
+		this.combo = combo;
+		this.combo.setPromptText("Escolha um arquivo");
+		this.combo.getItems().addAll(searchFiles());
+		this.combo.setOnAction(e->{
+			onClickComboBox(this.combo);
+		});
+		return this.combo;
+	}
+	
+	private void onClickComboBox(ComboBox<String> combo){
+		message = files.get(combo.getSelectionModel().getSelectedIndex());
+	}
+	
+	private List<String> searchFiles(){
+		files = new ArrayList<String>();
+		Reader rd = new Reader();
+		String path = rd.searchFile("");
+		File[] listfiles = new File(path).listFiles();
+		for (File file : listfiles) {
+		    if (file.isFile()) {
+		        files.add(file.getName());
+		    }
+		}		
+		return files;
+	}
+	
+	private Menu createMenuFile(){
+		menuFile = new Menu("Search File");
+		menuFile.getItems().add(createCustomComboBox(createComboBox(new ComboBox<String>())));
+		return menuFile;
+	}
+	
+	private CustomMenuItem createCustomTextField(TextField txt){
+		CustomMenuItem customMenuFileItem = new CustomMenuItem(txt);
+		customMenuFileItem.setHideOnClick(false);
+		return customMenuFileItem;
+	}
+	
+	private TextField createTextField(){
+		timeTxt = new TextField("em milisegundos");
+		timeTxt.setOnMouseClicked(e->{ 
+			onMouseClicked(timeTxt);
+		});
+		timeTxt.setOnKeyPressed(e->{
+			if(e.getCode().toString() == "ENTER"){
+				onKeyPressedTextfield();
+			}
+		});
+		return timeTxt;
+	}
+	
+	private void onKeyPressedTextfield(){
+		if (message == null){
+			onMouseClicked(this.timeTxt);
+		}
+		else{
+			message += ";" + timeTxt.getText();
+			menuConfig.hide();
+			ClientSocket client = new ClientSocket();
+			client.enviarDados("TESTANDO!!!");
+			//send the message to server 
+		}
+	}
+	
 	private void onMouseClicked(TextField txt){
 		txt.setText("");	
 	}
 	
-<<<<<<< HEAD
-	private void readFilesPath(){
-		String pathh = searchFile();
-		//Path path = new Path();
-		//System.out.println("a pathh é: " + pathh);
-		
-		File directory = new File(pathh);
-        //get all the files from a directory
-		String[] names = directory.list();
-	    File[] fList = directory.listFiles();
-	    String tam = "" + names.length;
-	    System.out.println("tamanho: " + tam);
-	    /*for (File file : fList){
-	        if (file.isFile()){
-	            System.out.println(file.getName());
-	        }
-	    }*/		
-	}
-	
-
-	
-	
-	private String searchFile(){
-		String path = System.getProperty("user.dir");
-		path += "\\Texts";
-		path = formatPath(path);
-		return path;
-	}
-	
-	private String formatPath(String filePath){
-		String newPath = filePath;
-		char barra = '\'';
-		String novo = System.getProperty("file.separator");
-		newPath.replace(barra,novo.charAt(0));
-		return newPath;
-	}
-	
-	private Menu createMenuFile(){
-		
-		menuFile = new Menu("Search File");
-		ComboBox fileCombo = new ComboBox();
-		
-		readFilesPath();
-		
-		fileCombo.getItems().addAll(
-	            "jacob.smith@example.com",
-	            "isabella.johnson@example.com",
-	            "ethan.williams@example.com",
-	            "emma.jones@example.com",
-	            "michael.brown@example.com");
-		
-		fileTxt = new TextField("nome do arquivo");
-		
-		
-		
-		menuFile.getItems().add(createCustomMenuItem(fileCombo));
-=======
-	private Menu createMenuFile(){
-		
-		menuFile = new Menu("Search File");
-		fileTxt = new TextField("nome do arquivo");
-		menuFile.getItems().add(createCustomMenuItem(fileTxt));
->>>>>>> master
-		
-		fileTxt.setOnMouseClicked(e->{
-			onMouseClicked(fileTxt);
-		});
-		
-		//when the enter is pressed, the value of time is submit
-		fileTxt.setOnKeyPressed(e->{
-			/*if(e.getCode().toString() == "ENTER"){
-				onValidateTime(timeTxt.getText());
-				rd.criarAnimacao(words, tempo, visor);
-				menuConfig.hide();
-			}*/
-		});
-		return menuFile;
-	}
-	
 	private Menu createMenuTime(){
 		menuTime = new Menu("Insert Time");
-		timeTxt = new TextField("em milisegundos");
-		
-<<<<<<< HEAD
-		//menuTime.getItems().add(createCustomMenuItem(timeTxt));
-=======
-		menuTime.getItems().add(createCustomMenuItem(timeTxt));
->>>>>>> master
-		
-		timeTxt.setOnMouseClicked(e->{ 
-			onMouseClicked(timeTxt);
-		});
-		
-		//when the enter is pressed, the value of time is submit
-		timeTxt.setOnKeyPressed(e->{
-			/*if(e.getCode().toString() == "ENTER"){
-				onValidateTime(timeTxt.getText());
-				rd.criarAnimacao(words, tempo, visor);
-				menuConfig.hide();
-			}*/
-		});
+		menuTime.getItems().add(createCustomTextField(createTextField()));
 		return menuTime;
 	}
 
