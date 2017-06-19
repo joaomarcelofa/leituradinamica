2d9396525c2c92260e9c3e695577b549b1ae6fd9
joaomarcelofa <joaov7.1996@gmail.com>
2017-06-19 20:16:25 -0300

    Criado classe de SQLite com um bd de Log

---
+++ LeituraDinamicaServer/src/sqlite/Main.java
@@ -0,0 +1,12 @@
+package sqlite;
+
+public class Main {
+
+	public static void main(String args[]) {
+		sqlOperations sql = new sqlOperations();
+		//sql.insert(20, "Teste 3", "POKEMON");
+		//sql.insert(22, "Teste 4", "We are the stars");
+		
+	}
+	
+}
